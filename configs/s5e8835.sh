ab0875d0e71f5eb5cd1a46b09b5773084d26ac70
Majaahh <maja.blaszczak@proton.me>
2026-04-12 21:27:28 +0200

    samsung: Initial s5e8835 configuration
    Change-Id: I4d9a6e17f7fd4d0c010c2c8ac7c41025dcf2164e
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ configs/s5e8835.sh
@@ -0,0 +1,8 @@
+# shellcheck disable=SC2034 shell=bash
+#
+# SPDX-FileCopyrightText: Majaahh
+# SPDX-License-Identifier: Apache-2.0
+#
+
+AUDIO_BLOBS=( "calliope_sram.bin" "vts.bin" )
+FIRMWARE_BLOBS=( "AIE.bin" "mfc_fw.bin" "os.checked.bin" "pablo_icpufw.bin" )
